fa95c1b1524aea2ea3e27f0a1e8766dd5e64e3b3
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-11-23 20:28:57 -0600

    "Create initial Django app"

---
+++ ProyectoDeUnidadTres/AmiGo/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sofia\Desktop\UNAM_TEC\GitHub\PracticasComputacionI\ProyectoDeUnidadTres\AmiGo\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
